df04349c62cc5d12f5eeadfaa38f333a2558fabc
YueHongjie <hongjie.yue@ele.me>
2017-05-08 18:34:12 +0800

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yuehongjie on 17/5/8.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+    }
+}
